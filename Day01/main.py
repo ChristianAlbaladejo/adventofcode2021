6d9297c1cb47a9c56b39ecda88a4199986ece4b1
christian.albaladejo06@gmail.com <christian.albaladejo06@gmail.com>
2021-12-01 14:36:15 +0100

    day 1

---
+++ Day01/main.py
@@ -0,0 +1,51 @@
+f = open("input.txt", "r")
+data = list(map(int, f.readlines()))
+
+
+def part_1():
+    total = 0
+    for idx, x in enumerate(data):
+        if idx == 0:
+            print("(N/A - no previous measurement)")
+        elif data[idx - 1] < x:
+            print("(increased)")
+            total += 1
+        else:
+            print("(decreased)")
+
+    print("--------")
+    print("Total: " + str(total))
+    print("--------")
+    f.close()
+
+
+def part_2():
+    data2 = [199,200,208,210,200,207,240,269,260,263]
+    lastSum = 0
+    total = 0
+    for idx, x in enumerate(data):
+        if idx == 0:
+            print("(N/A - no previous measurement)")
+        else:
+            try:
+                number = data[idx] + data[idx + 1] + data[idx + 2]
+                if (lastSum > number):
+                    print(str(idx) + ": " + str(data[idx]) + "+" + str(data[idx + 1]) + "+" + str(
+                        data[idx + 2]) + " = " + str(number) + "(decreased)")
+                elif (lastSum < number):
+                    total += 1
+                    print(str(idx) + ": " + str(data[idx]) + "+" + str(data[idx + 1]) + "+" + str(
+                        data[idx + 2]) + " = " + str(number) + "(increased)")
+                else:
+                    print(str(idx) + ": " + str(data[idx]) + "+" + str(data[idx + 1]) + "+" + str(
+                        data[idx + 2]) + " = " + str(number) + " - " + str(lastSum) + "(no change)")
+                lastSum = number
+            except:
+                print("out")
+    print("--------")
+    print("Total: " + str(total))
+    print("--------")
+    f.close()
+if __name__ == "__main__":
+    # part_1()
+    part_2()
